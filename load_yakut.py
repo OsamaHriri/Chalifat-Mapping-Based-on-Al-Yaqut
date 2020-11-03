67df280c0225913e4634369f73857e22991a5e31
Tomer Sagi <ts.tomersagi@gmail.com>
2020-11-03 11:24:24 +0200

    Yakut XLSX to MariaDB load script

---
+++ load_yakut.py
@@ -0,0 +1,5 @@
+# Run schema creation script
+
+# Convert distinct values in TypeAr and TypeEng to table PlaceTypes and replace with foreign key. 
+
+# Remove the letter Q from the WikidataID field before converting to int and inserting
